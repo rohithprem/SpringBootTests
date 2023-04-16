1584aed806936991d656e87c0925e6a8cf8b9270
Rohith Premachandran <rohithp@live.com>
2023-04-16 23:50:25 +0200

    Initial Spring Boot Template Commit

---
+++ src/test/java/com/springboottest/rohith/springboottest/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboottest.rohith.springboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
